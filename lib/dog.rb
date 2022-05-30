a25a96f26482c713bfe854afd7a8cb45e1ab6d82
Malcolm Keyes <3.malcolmk@gmail.com>
2022-05-30 14:12:22 -0400

    commit tree f3f144e8080fb04141ca43b1d9d4681bbab0fced

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog
+    # doggo
+
+    def name=(dog_name)
+        @name=dog_name
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
+
+    def bark
+        puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+end
---
+++ lib/person.rb
@@ -0,0 +1,27 @@
+class Person
+    # peeps!
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+    
+    def job
+        @job
+    end
+
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+end
